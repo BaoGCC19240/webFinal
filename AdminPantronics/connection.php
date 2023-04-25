be2adf4efd05434158749a8f7aa5dfa6bcb235bb
BaoGCC19240 <91667147+BaoGCC19240@users.noreply.github.com>
2023-04-25 15:35:10 +0700

    Update connection.php

--- AdminPantronics/connection.php
+++ AdminPantronics/connection.php
@@ -1,4 +1,4 @@
 <?php 
-$conn = mysqli_connect('localhost','root','zxcvBnm123','pantronics')
+$conn = mysqli_connect('us-cdbr-east-06.cleardb.net','b5fea4cf3ece7f','d6ec4ae6','heroku_063d4d819c34d46')
 or die("Can not connect database".mysqli_connect_error());
 ?>
